ad9a951bacade3ae1df4facabbf4e8f386968c7d
manishjayan <manishjayan@gmail.com>
2018-02-15 09:51:26 +0530

    added layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Sangrah</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<link
         <!-- Styles -->
         <style>
             html, body {
@@ -19,6 +20,10 @@
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
+            }
+            #icon{
+            	font-size:25px;
+            	font-weight:bold;
             }
 
             .full-height {
@@ -63,6 +68,44 @@
                 margin-bottom: 30px;
             }
         </style>
+	<div class="container">
+	    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a id="icon" class="navbar-brand" href="{{ url('/') }}">
+                        Sangrah
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
+
+                    <!-- Right Side Of Navbar --><b>
+                    <ul class="nav navbar-nav navbar-left">
+                        <!-- Authentication Links -->
+                            <li><a href="">Leaderboard</a></li>
+                            <li><a href="">Winners</a></li>
+                    </ul>
+			</b>
+                </div>
+            </div>
+        </nav>
+    </div>
+</div>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
